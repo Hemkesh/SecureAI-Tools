60885f8269fef2535687834e2f2b08140894b313
Hemkesh <hemkesh@tryvillage.co>
2024-06-26 21:20:46 +0000

    Fix GA analytics ID issue

--- apps/web/pages/google-analytics.tsx
+++ apps/web/pages/google-analytics.tsx
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import Script from 'next/script';
+import { GA_ID } from './google-analytics-id';
 
 const GoogleAnalytics = () => {
   return (
     <>
       <Script
         strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
       />
 
       <Script id='' strategy='lazyOnload'>
@@ -16,7 +17,7 @@ const GoogleAnalytics = () => {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              gtag('config', '${GA_ID}', {
               page_path: window.location.pathname,
               });
           `}
